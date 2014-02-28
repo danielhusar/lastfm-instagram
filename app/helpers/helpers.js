60eeb8e0b1710a1b244102f9f97ac1301ee6fd85
Daniel Husar <dano.husar@gmail.com>
2014-02-28 11:38:12 +0000

    integrate gulp

--- app/helpers/helpers.js
+++ app/helpers/helpers.js
@@ -1,116 +1,119 @@
-'use strict';
+(function(){
+  'use strict';
 
-var fs = require('fs');
+  var fs = require('fs');
 
-/*
- * Generate template
- * @param(res) res
- * @param(template)
- *
- */
-exports.template = function(res, template, variables){
-	variables.enviroment = (process.env.NODE_ENV === 'production') ? process.env.NODE_ENV : 'development';
-	res.render(template, variables);
-};
+  /*
+   * Generate template
+   * @param(res) res
+   * @param(template)
+   *
+   */
+  exports.template = function(res, template, variables){
+    variables.isProduction = process.env.NODE_ENV === 'production';
+    res.render(template, variables);
+  };
 
-
-/**
- * Cache helpers
- */
-var cache = exports.cache = {
 
   /**
-   * Base path for caching for our app
-   * @type {string}
+   * Cache helpers
    */
-  baseCachePath :  process.cwd() + '/cache/',
+  var cache = exports.cache = {
 
-  /**
-   * Check if the cache for the specified file exist, adn return it if yes
-   * @param  {string} fileName to check
-   * @param  {number} time     time in second to check
-   * @return {boolean/json}
-   */
-  returnCache: function(fileName, time){
-    var fileUpdate = cache.readCache(fileName).mtime || false,
-        lastUpdate = fileUpdate ? (new Date().getTime() - new Date(fileUpdate).getTime()) / 1000 : false,
-        data;
-    if(lastUpdate && lastUpdate < time){
-      data = cache.getCache(fileName);
-      if(data){
-        return JSON.parse(data);
+    /**
+     * Base path for caching for our app
+     * @type {string}
+     */
+    baseCachePath :  process.cwd() + '/cache/',
+
+    /**
+     * Check if the cache for the specified file exist, adn return it if yes
+     * @param  {string} fileName to check
+     * @param  {number} time     time in second to check
+     * @return {boolean/json}
+     */
+    returnCache: function(fileName, time){
+      var fileUpdate = cache.readCache(fileName).mtime || false,
+          lastUpdate = fileUpdate ? (new Date().getTime() - new Date(fileUpdate).getTime()) / 1000 : false,
+          data;
+      if(lastUpdate && lastUpdate < time){
+        data = cache.getCache(fileName);
+        if(data){
+          return JSON.parse(data);
+        }else{
+          return false;
+        }
       }else{
         return false;
       }
-    }else{
-      return false;
+    },
+
+    /**
+     * Returns the fullfile path
+     * @param  {file} file - file name
+     * @return {string} full file path
+     */
+    basePathFile : function(file){
+      file = string.slug(file);
+      return (cache.baseCachePath + file + '.json');
+    },
+
+    /**
+     * Get the cached file
+     * @param  {file} file - file name
+     * @return {object} file content or false if it doesnt exists
+     */
+    getCache : function(file){
+      file = cache.basePathFile(file);
+      return fs.existsSync(file) ? fs.readFileSync(file, 'utf8' ) : false;
+    },
+
+    /**
+     * Read the file properties
+     * @param  {string} file - file name
+     * @return {object} file properties or false if it doesnt exists
+     */
+    readCache : function(file){
+      file = cache.basePathFile(file);
+      return fs.existsSync(file) ? fs.statSync(file) : false;
+    },
+
+    /**
+     * Store data in file
+     * @param  {filenam} file - file name
+     * @param  {object} data to storeCache
+     * @return {void}
+     */
+    storeCache : function(file, data){
+      if(typeof data === 'object'){
+        data = JSON.stringify(data);
+      }
+      if(!data || data === '[]' || data === '{}'){
+        return;
+      }
+      file = cache.basePathFile(file);
+      fs.writeFileSync(file, data);
     }
-  },
+  };
 
-  /**
-   * Returns the fullfile path
-   * @param  {file} file - file name
-   * @return {string} full file path
-   */
-  basePathFile : function(file){
-    file = string.slug(file);
-    return (cache.baseCachePath + file + '.json');
-  },
+  var string = exports.string = {
+    slug: function(str) {
+      str = str.replace(/^\s+|\s+$/g, ''); // trim
+      str = str.toLowerCase();
 
-  /**
-   * Get the cached file
-   * @param  {file} file - file name
-   * @return {object} file content or false if it doesnt exists
-   */
-  getCache : function(file){
-    file = cache.basePathFile(file);
-    return fs.existsSync(file) ? fs.readFileSync(file, 'utf8' ) : false;
-  },
+      // remove accents, swap ñ for n, etc
+      var from = 'ãàáäâẽèéëêìíïîõòóöôùúüûñçľščťžýň·/_,:;';
+      var to   = 'aaaaaeeeeeiiiiooooouuuunclsctzyn------';
+      for (var i=0, l=from.length ; i<l ; i++) {
+        str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+      }
 
-  /**
-   * Read the file properties
-   * @param  {string} file - file name
-   * @return {object} file properties or false if it doesnt exists
-   */
-  readCache : function(file){
-    file = cache.basePathFile(file);
-    return fs.existsSync(file) ? fs.statSync(file) : false;
-  },
+      str = str.replace(/[^a-z0-9 -]/g, '') // remove invalid chars
+        .replace(/\s+/g, '') // collapse whitespace and replace by space
+        .replace(/-+/g, '-'); // collapse dashes
 
-  /**
-   * Store data in file
-   * @param  {filenam} file - file name
-   * @param  {object} data to storeCache
-   * @return {void}
-   */
-  storeCache : function(file, data){
-    if(typeof data === 'object'){
-      data = JSON.stringify(data);
+      return str;
     }
-    if(!data || data === '[]' || data === '{}'){
-      return;
-    }
-    file = cache.basePathFile(file);
-    fs.writeFileSync(file, data);
-  }
-};
+  };
 
-var string = exports.string = {
-  slug: function(str) {
-    str = str.replace(/^\s+|\s+$/g, ''); // trim
-    str = str.toLowerCase();
-
-    // remove accents, swap ñ for n, etc
-    var from = 'ãàáäâẽèéëêìíïîõòóöôùúüûñçľščťžýň·/_,:;';
-    var to   = 'aaaaaeeeeeiiiiooooouuuunclsctzyn------';
-    for (var i=0, l=from.length ; i<l ; i++) {
-      str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
-    }
-
-    str = str.replace(/[^a-z0-9 -]/g, '') // remove invalid chars
-      .replace(/\s+/g, '') // collapse whitespace and replace by space
-      .replace(/-+/g, '-'); // collapse dashes
-
-    return str;
-  }
-};
+})();
---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+'use strict';
+
+// Include gulp
+var gulp = require('gulp');
+
+// Include Our Plugins
+var jshint = require('gulp-jshint');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var ngmin = require('gulp-ngmin');
+var less = require('gulp-less');
+var cssmin = require('gulp-cssmin');
+var prefix = require('gulp-autoprefixer');
+var stylish = require('jshint-stylish');
+var rename = require('gulp-rename');
+
+//less
+gulp.task('less', function () {
+  gulp.src('less/style.less')
+    .pipe(less())
+    .pipe(prefix('last 2 version', 'ie 8', 'ie 9')).pipe(gulp.dest('public/css'))
+    .pipe(gulp.dest('./public/css'));
+});
+
+//minify css
+gulp.task('cssmin', function () {
+  gulp.src(['public/css/style.css']).pipe(cssmin({
+    expand: true,
+    keepSpecialComments: 0,
+    noAdvanced: true
+  })).pipe(rename({
+    suffix: '.min',
+    extname: '.css'
+  })).pipe(gulp.dest('public/css'));
+});
+
+//lint javascript
+gulp.task('lint', function () {
+  gulp.src(['app/**/*.js', 'public/js/**/*.js', '!public/js/**/*.min.js', '!public/components/**/**'])
+    .pipe(jshint())
+    .pipe(jshint.reporter(stylish));
+});
+
+//concatenate & minify JS
+gulp.task('scripts', function () {
+  gulp.src(['public/js/app.js'])
+    .pipe(concat('app.min.js'))
+    .pipe(ngmin())
+    .pipe(uglify())
+    .pipe(gulp.dest('public/js'));
+});
+
+//watch Files For Changes
+gulp.task('watch', function () {
+  gulp.watch(['less/*.less', 'less/**/*.less', 'less/**/**/*.less', 'less/**/**/**/*.less'], ['less']);
+});
+
+//tasks aliases
+gulp.task('default', ['scripts', 'less', 'cssmin']);
+gulp.task('server', ['watch']);
